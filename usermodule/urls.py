932e23da2b3e29c68486c6d051b2cee90947581f
unknown <daivalokash22@gmail.com>
2024-04-26 22:46:16 +0530

    uwu]

---
+++ usermodule/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from .import views
+urlpatterns = [
+    path("checkuserlogin",views.checkuserlogin,name="checkuserlogin"),
+    path("userhome",views.userhome,name="userhome"),
+    path("viewdonations",views.viewdonations,name="viewdonations"),
+    path("userchangepwd",views.userchangepwd, name="userchangepwd"),
+   path("userupdatepwd",views.userupdatepwd,name="userupdatepwd"),
+    path("contactmail/", views.contactmail, name="contactmail"),
+    path("feedback",views.feedback,name="feedback"),
+    path("feedus/",views.feedus,name="feedus"),
+    path("contactfeed",views.conatctfeed,name="contactfeed"),
+]
